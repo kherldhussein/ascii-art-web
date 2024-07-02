a34cfa15c1b0c222a65d2a356205a92438ffb067
khahussein <kherld.hussein@gmail.com>
2024-07-02 03:46:30 +0300

    Feat: Add middleware

--- server/server.go
+++ server/server.go
@@ -16,7 +16,20 @@ var banners = map[string]string{
 	"shadow":     "shadow.txt",
 }
 
+// Enable CORS middleware
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func AsciiServer(w http.ResponseWriter, r *http.Request) {
+	// Handle CORS preflight request
+	if r.Method == "OPTIONS" {
+		enableCors(&w)
+		return
+	}
+
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
