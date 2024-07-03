138736c9747d9509e9d298e4b5fd18cf3c401914
Tomlee Abila <tomleeabila@gmail.com>
2024-07-03 23:37:46 +0300

    refactor: handle non-printable charactors

--- printAscii/print_ascii.go
+++ printAscii/print_ascii.go
@@ -1,49 +1,38 @@
 package webAscii
 
 import (
-	"fmt"
-	"net/http"
 	"strings"
 )
 
-func printWord(w http.ResponseWriter, word string, asciiArtGrid [][]string) string {
+func printWord(word string, asciiArtGrid [][]string) string {
 	result := ""
 	for i := 1; i <= 8; i++ {
 		for _, char := range word {
 			index := int(char - 32)
-			if index < 0 || index >= len(asciiArtGrid) {
-				http.Error(w, "non-printable ascii character", http.StatusMethodNotAllowed)
-				return ""
-			} else {
-				result += asciiArtGrid[index][i]
-			}
+			result += asciiArtGrid[index][i]
 		}
 		result += "\n"
 	}
 	return result
 }
 
-func PrintArt(w http.ResponseWriter, str string, asciiArtGrid [][]string) string {
+func PrintArt(str string, asciiArtGrid [][]string) string {
 	result := ""
 	switch str {
 	case "":
-		fmt.Print()
+		result += ""
 	case "\\n":
-		fmt.Println()
-	case "\\r", "\\f", "\\v", "\\t", "\\b", "\\a":
-		http.Error(w, "Unsupported escape sequence", http.StatusMethodNotAllowed)
-		return ""
+		result += "\n"
 	default:
 		s := strings.ReplaceAll(str, "\\n", "\n")
-		s = strings.ReplaceAll(s, "\\r", "\r")
-		s = strings.ReplaceAll(s, "\\f", "\f")
-		s = strings.ReplaceAll(s, "\\v", "\v")
-		s = strings.ReplaceAll(s, "\\t", "\t")
-		s = strings.ReplaceAll(s, "\\b", "\b")
-		s = strings.ReplaceAll(s, "\\a", "\a")
 		words := strings.Split(s, "\n")
 		num := 0
 		for _, word := range words {
+			for _, ch := range word {
+				if ch < ' ' || ch > '~' {
+					return string(ch) + " is non-printable ascii character"
+				}
+			}
 			if word == "" {
 				num++
 				if num < len(words) {
@@ -51,7 +40,7 @@ func PrintArt(w http.ResponseWriter, str string, asciiArtGrid [][]string) string
 					continue
 				}
 			} else {
-				return printWord(w, word, asciiArtGrid)
+				result += printWord(word, asciiArtGrid)
 			}
 		}
 	}
