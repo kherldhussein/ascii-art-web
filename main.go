39abce995b3453b62bd5f0c307fb0b240292efab
doonyango <datieno001@gmail.com>
2024-07-03 16:22:08 +0300

    Feat: Implement server link

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	http.Handle("/", fileserver)
 	http.HandleFunc("/ascii", server.AsciiServer)
 
+	log.Println("Server listening on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatalf("Error Running Server: %v ", err)
 	}
