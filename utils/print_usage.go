5351c40573915279363245c280efa6122ab78550
khahussein <kherld.hussein@gmail.com>
2024-07-08 17:57:34 +0300

    Feat: Implement custom error handler func. works with http to return a desired message

--- utils/print_usage.go
+++ utils/print_usage.go
@@ -1,8 +1,15 @@
 package webAscii
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func PrintUsage() {
 	fmt.Print("Usage: go run . [OPTION] [STRING] [BANNER]\n\n")
 	fmt.Println("EX: go run . --output=<fileName.txt> something standard")
+}
+
+func SendError(w http.ResponseWriter, message string, status int) {
+	http.Error(w, message, status)
 }
