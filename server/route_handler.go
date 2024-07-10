4f5efb0b0c9a86dae374e3e9933a8d1e4f403510
doonyango <datieno001@gmail.com>
2024-07-10 18:34:55 +0300

    Fix: Add 404 hander page

--- server/route_handler.go
+++ server/route_handler.go
@@ -1,19 +1,47 @@
 package webAscii
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+
+	send "webAscii/utils"
 )
 
 func Handl(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	if r.URL.Path == "/" {
+		tmpl, err := template.ParseFiles("./templates/index.html")
+		if err != nil {
+			tmpl, err := template.ParseFiles("./templates/404.html")
+			if err != nil {
+				send.SendError(w, "Internal Sasdfgdfgerver Error", http.StatusInternalServerError)
+				return
+			}
+
+			if err = tmpl.Execute(w, nil); err != nil {
+				fmt.Printf("error %v", err)
+			}
+			// send.SendError(w, "Error 404: NOT FOUND", http.StatusNotFound)
+			http.NotFound(w, r)
+			return
+		}
+
+		if err = tmpl.Execute(w, nil); err != nil {
+			fmt.Printf("error %v", err)
+		}
+
 	}
-	tmpl, err := template.ParseFiles("./templates/index.html")
+
+	tmpl, err := template.ParseFiles("./templates/404.html")
 	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		send.SendError(w, "Internal Sasdfgdfgerver Error", http.StatusInternalServerError)
 		return
 	}
-	err = tmpl.Execute(w, nil)
+
+	if err = tmpl.Execute(w, nil); err != nil {
+		fmt.Printf("error %v", err)
+	}
+	// send.SendError(w, "Error 404: NOT FOUND", http.StatusNotFound)
+	http.NotFound(w, r)
+	return
 }
